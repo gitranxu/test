322a1299d71a1e308301e1303f836ea9a1c9b4a5
ranxu <ranxu07_18@163.com>
2015-09-27 10:34:40 +0800

    增加a.js,b.js,c.js三个测试文件，在本地nignx上跑通success函数

---
+++ data/b.js
@@ -0,0 +1,8 @@
+{
+	"cities":[
+		{"name" : "保定","value":1},
+		{"name" : "石家庄","value":2},
+		{"name" : "承德","value":3},
+		{"name" : "邢台","value":4}
+	]
+}
---
+++ data/c.js
@@ -0,0 +1,7 @@
+{
+	"cities":[
+		{"name" : "沈阳","value":1},
+		{"name" : "大连","value":2},
+		{"name" : "铁岭","value":3}
+	]
+}
